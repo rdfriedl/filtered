252a3ac2e3fca222804d3fe46ede452b63233f04
Robert Friedl <rdfriedl@gmail.com>
2016-10-16 18:48:15 -0500

    fixed bugs with starting server on openshift

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 
-const port = process.env.PORT || 8080;
 var app = express();
 
 // add the api
 app.use('/', require('./app/index.js'));
 app.use('/api', require('./api/index.js'));
 
+// openshift health report
+app.get('/health', function(req, res){
+	res.send('hello world');
+});
+
 // start
-app.listen(port);
+const port = process.env.NODE_PORT || 8080;
+const host = process.env.NODE_IP || 'localhost';
+app.listen(port, host);
 console.log('server started on port: ' + port);

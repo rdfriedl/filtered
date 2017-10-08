fc77b90dc97539c7c1db642a15c6fe0fa6046ef1
rdfriedl <rdfriedl@gmail.com>
2017-10-07 19:17:32 -0500

    added circleci

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,18 @@
 /* jshint node: true */
 
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var uglifyjs = require('gulp-uglifyjs');
-var uglifycss = require('gulp-uglifycss');
-var concat = require('gulp-concat');
-var minifyHTML = require('gulp-minify-html');
-var header = require('gulp-header');
-var replace = require('gulp-replace');
-var gulpIgnore = require('gulp-ignore');
-var order = require("gulp-order");
-var wrap = require("gulp-wrap");
-var closureCompiler = require('google-closure-compiler').gulp();
-var webserver = require('gulp-webserver');
+let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let uglifycss = require('gulp-uglifycss');
+let concat = require('gulp-concat');
+let minifyHTML = require('gulp-minify-html');
+let header = require('gulp-header');
+let replace = require('gulp-replace');
+let gulpIgnore = require('gulp-ignore');
+let order = require("gulp-order");
+let webserver = require('gulp-webserver');
 
-var pkg = require('./package.json');
-var banner = ['/**',
+let pkg = require('./package.json');
+let banner = ['/**',
   ' * <%= pkg.name %> - v<%= pkg.version %>',
   ' */',
   ''].join('\n');
@@ -31,76 +28,9 @@ gulp.task('compile-js',function(){
 			'*outputs*',
 			'*page*'
 		]))
-		// .pipe(uglifyjs('app.js', {
-	 //    	mangle: {
-		//         except      : [],
-		//         eval        : true,
-		//         sort        : true,
-		//         toplevel    : false,
-		//         screw_ie8   : false,
-		//         keep_fnames : true
-	 //    	}
-	 //    }))
 		.pipe(concat('dist/app.js'))
-    	.pipe(uglify({
-    		mangle: {
-		        except      : [],
-		        eval        : true,
-		        sort        : true,
-		        toplevel    : false,
-		        screw_ie8   : false,
-		        keep_fnames : true
-	    	},
-    		compress: {
-    			sequences     : true,  // join consecutive statemets with the “comma operator”
-				properties    : true,  // optimize property access: a["foo"] → a.foo
-				dead_code     : true,  // discard unreachable code
-				drop_debugger : true,  // discard “debugger” statements
-				unsafe        : false, // some unsafe optimizations (see below)
-				conditionals  : true,  // optimize if-s and conditional expressions
-				comparisons   : true,  // optimize comparisons
-				evaluate      : true,  // evaluate constant expressions
-				booleans      : true,  // optimize boolean expressions
-				loops         : true,  // optimize loops
-				unused        : true,  // drop unused variables/functions
-				hoist_funs    : true,  // hoist function declarations
-				hoist_vars    : true, // hoist variable declarations
-				if_return     : true,  // optimize if-s followed by return/continue
-				join_vars     : true,  // join var declarations
-				cascade       : true,  // try to cascade `right` into `left` in sequences
-				side_effects  : true,  // drop side-effect-free statements
-				warnings      : false,  // warn about potentially dangerous optimizations/code
-				global_defs   : {}     // global definitions
-    		}
-    	}))
-		// .pipe(wrap('(function(){<%= contents %>})()'))
-		/*.pipe(closureCompiler({
-			compilation_level: 'ADVANCED',
-			warning_level: 'QUIET',
-			language_in: 'ECMASCRIPT6_STRICT',
-			language_out: 'ECMASCRIPT5_STRICT',
-			output_wrapper: '(function(){\n%output%\n}).call(this)',
-    		js_output_file: 'app.js',
-    		externs: [
-				'lib/bootstrap.min.js',
-				'lib/knockout-min.js',
-    			'lib/Dexie.min.js',
-				'lib/ZeroClipboard.Core.min.js',
-				'lib/ZeroClipboard.min.js',
-				'lib/dom.jsPlumb-1.7.5-min.js',
-				'lib/jquery.js',
-				'lib/jquery.mousewheel.min.js',
-				'lib/jquery.transit.min.js',
-				'lib/prettify.js',
-				'lib/svg.draggable.min.js',
-				'lib/svg.filter.min.js',
-				'lib/svg.js',
-				'lib/svg.resize.min.js',
-				'lib/svg.select.min.js'
-			]
-        }))*/
-  		.pipe(header(banner, { pkg : pkg } ))
-  		//.pipe(gulp.dest('dist/'));
+		.pipe(uglify())
+		.pipe(header(banner, { pkg : pkg } ))
 		.pipe(gulp.dest('./'));
 });
 
@@ -124,7 +54,6 @@ gulp.task('compile-css',function(){
   		.pipe(header(banner, { pkg : pkg } ))
 		.pipe(gulp.dest('./'));
 });
-var header = require('gulp-header');
 
 gulp.task('compile-html',function(){
 	return gulp.src('index.html')
@@ -179,7 +108,7 @@ gulp.task('watch',function(){
         'help/*',
         'help/help_files/*'
     ],['compile-data']);
-})
+});
 
 gulp.task('serve', function(){
 	return gulp.src('./')
@@ -187,7 +116,7 @@ gulp.task('serve', function(){
 			livereload: true,
 			open: true
 	    }));
-})
+});
 
 gulp.task('serve-dist', function(){
 	return gulp.src('./dist/')
@@ -195,6 +124,6 @@ gulp.task('serve-dist', function(){
 			livereload: true,
 			open: true
 	    }));
-})
+});
 
 gulp.task('default',['watch','serve-dist']);
